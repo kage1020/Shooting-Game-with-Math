b4daadd175e98b692d13fcf3677ae6c06e8476a1
kage1020 <73168765+kage1020@users.noreply.github.com>
2024-02-27 15:15:18 +0900

    update dir structure

---
+++ Resource.h
@@ -0,0 +1,38 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// Shooting-game-with-Math.rc で使用
+//
+#define IDC_MYICON                      2
+#define IDD_SHOOTINGGAMEWITHMATH_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SHOOTINGGAMEWITHMATH        107
+#define IDI_SMALL                       108
+#define IDC_SHOOTINGGAMEWITHMATH        109
+#define IDR_MAINFRAME                   128
+#define IDR_ACCELERATOR1                129
+#define IDC_STATIC                      -1
+
+#define ROCKET_BODY                     201
+#define ROCKET_SUB2                     202
+#define ROCKET_SUB3                     203
+#define ROCKET_SUB4                     204
+#define ROCKET_SUB5                     205
+#define ROCKET_SUB6                     206
+#define ROCKET_SUB7                     207
+#define ROCKET_SUB8                     208
+#define ROCKET_SUB9                     209
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
